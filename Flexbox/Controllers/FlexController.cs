2fe649de7a73f590ad9f66a39193107c4f567b2f
weichien <ywc92@hotmail.com>
2022-10-23 02:25:02 +0800

    first commit

---
+++ Flexbox/Controllers/FlexController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flexbox.Controllers
+{
+    public class FlexController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Console download PDF file
